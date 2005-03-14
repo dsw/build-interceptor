576ac2c40b8c2acc5ca4f3133cff12f951ddc989
Karl Chen <quarl@cs.berkeley.edu>
2005-03-14 19:43:42 +0000

    Allow .sho extension
    git-svn-id: http://build-interceptor.tigris.org/svn/build-interceptor/trunk@84 8700b9f2-3af6-0310-88e9-a4e0a7708a09

--- collect2_interceptor.pl
+++ collect2_interceptor.pl
@@ -239,7 +239,7 @@ for my $line (split '\n', $trace_output0) {
       # ignore for now
       next;
       # $file = $1;
-  } elsif ($line =~ m/^(.+\.(?:o|os|oS|lo))$/) {
+  } elsif ($line =~ m/^(.+\.(?:o|os|oS|lo|sho))$/) {
       # a .o file not from an archive, like this:
       #   /usr/lib/crt1.o
       # Can also include .lo (libtool object) files.
