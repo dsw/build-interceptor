a0382ff1536ed404a5e1dda1cec199871c37f448
Karl Chen <quarl@cs.berkeley.edu>
2005-03-20 22:37:06 +0000

    Handle .po extensions
    git-svn-id: http://build-interceptor.tigris.org/svn/build-interceptor/trunk@104 8700b9f2-3af6-0310-88e9-a4e0a7708a09

--- collect2_interceptor.pl
+++ collect2_interceptor.pl
@@ -238,7 +238,7 @@ if (!-e $outfile_abs) {
 }
 
 my $executable = -x $outfile_abs &&
- $outfile_abs !~ /[.](?:so(?:[.]\d+)*|la|al|o|lo|os|oS|opic|sho)$/;
+ $outfile_abs !~ /[.](?:so(?:[.]\d+)*|la|al|o|lo|os|oS|po|opic|sho)$/;
 
 if (do_not_add_interceptions_to_this_file($outfile_abs)) {
     # Don't add .note.ld_interceptor, and in addition, remove
@@ -310,7 +310,7 @@ for my $line (split '\n', $trace_output0) {
       # ignore for now
       next;
       # $file = $1;
-  } elsif ($line =~ m/^([^()]+\.(?:o|os|oS|lo|sho|opic))$/) {
+  } elsif ($line =~ m/^([^()]+\.(?:o|os|oS|lo|sho|po|opic))$/) {
       # a .o file not from an archive, like this:
       #   /usr/lib/crt1.o
       # Can also include .lo (libtool object) files.
