65a307fa4347871b82203108d983bedfd78333fa
Karl Chen <quarl@cs.berkeley.edu>
2006-04-05 02:25:50 +0000

    Formatting/comments
    git-svn-id: http://build-interceptor.tigris.org/svn/build-interceptor/trunk@238 8700b9f2-3af6-0310-88e9-a4e0a7708a09

--- make-spec-file.pl
+++ make-spec-file.pl
@@ -4,6 +4,10 @@
 
 # syntax: make-spec-file.pl interceptor.specs.in interceptor.specs-3.4
 
+# Note that we can't use cpp because it doesn't respect whitespace, and
+# whitespace is important.  We can't use m4 because we don't want to require
+# m4 in the chroots at build-interceptor installation time.
+
 use strict;
 use warnings;
 use FileHandle;
@@ -15,7 +19,7 @@ my $out = new FileHandle(">$output") or die $!;
 
 my ($ver) = ($output =~ m/-([0-9][.][0-9.]+)$/) or die;
 
-print $out "# specs for gcc $ver\n";
+print $out "### specs for gcc $ver\n";
 
 my $found = 0;
 
